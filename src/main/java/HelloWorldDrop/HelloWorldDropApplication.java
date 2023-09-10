6376c565b96f2206cefeb69c47848f82ae3c2c05
pedro <gil.catre@gmail.com>
2023-09-10 11:33:39 +0200

    adding dependencies

--- src/main/java/HelloWorldDrop/HelloWorldDropApplication.java
+++ src/main/java/HelloWorldDrop/HelloWorldDropApplication.java
@@ -1,11 +1,14 @@
 package HelloWorldDrop;
 
+import HelloWorldDrop.core.Person;
 import HelloWorldDrop.resources.HelloWorldResource;
 import io.dropwizard.core.Application;
 import io.dropwizard.core.setup.Bootstrap;
 import io.dropwizard.core.setup.Environment;
+import io.dropwizard.db.DataSourceFactory;
+import io.dropwizard.migrations.MigrationsBundle;
 import ru.vyarus.dropwizard.guice.GuiceBundle;
-
+import io.dropwizard.hibernate.HibernateBundle;
 public class HelloWorldDropApplication extends Application<HelloWorldDropConfiguration> {
 
     public static void main(final String[] args) throws Exception {
@@ -17,12 +20,27 @@ public class HelloWorldDropApplication extends Application<HelloWorldDropConfigu
         return "HelloWorldDrop";
     }
 
+
+    private final HibernateBundle<HelloWorldDropConfiguration> hibernate = new HibernateBundle<HelloWorldDropConfiguration>(Person.class) {
+        @Override
+        public DataSourceFactory getDataSourceFactory(HelloWorldDropConfiguration configuration) {
+            return configuration.getDataSourceFactory();
+        }
+    };
+
     @Override
     public void initialize(final Bootstrap<HelloWorldDropConfiguration> bootstrap) {
         // TODO: application initialization
         bootstrap.addBundle(GuiceBundle.builder()
                 .enableAutoConfig()
                 .build());
+
+        bootstrap.addBundle(new MigrationsBundle<HelloWorldDropConfiguration>() {
+            @Override
+            public DataSourceFactory getDataSourceFactory(HelloWorldDropConfiguration configuration) {
+                return configuration.getDataSourceFactory();
+            }
+        });
 
     }
 
@@ -31,6 +49,10 @@ public class HelloWorldDropApplication extends Application<HelloWorldDropConfigu
                     final Environment environment) {
         // TODO: implement application
         environment.jersey().register(new HelloWorldResource());
+
+//        final JdbiFactory factory = new JdbiFactory();
+//        final Jdbi jdbi = factory.build(environment, config.getDataSourceFactory(), "postgresql");
+//        environment.jersey().register(new UserResource(jdbi));
     }
 
 }
--- src/main/java/HelloWorldDrop/HelloWorldDropConfiguration.java
+++ src/main/java/HelloWorldDrop/HelloWorldDropConfiguration.java
@@ -2,9 +2,23 @@ package HelloWorldDrop;
 
 import io.dropwizard.core.Configuration;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.Valid;
 import org.hibernate.validator.constraints.*;
 import jakarta.validation.constraints.*;
+import io.dropwizard.db.DataSourceFactory;
 
 public class HelloWorldDropConfiguration extends Configuration {
-    // TODO: implement service configuration
+    @Valid
+    @NotNull
+    private DataSourceFactory database = new DataSourceFactory();
+
+    @JsonProperty("database")
+    public void setDataSourceFactory(DataSourceFactory factory) {
+        this.database = factory;
+    }
+
+    @JsonProperty("database")
+    public DataSourceFactory getDataSourceFactory() {
+        return database;
+    }
 }
